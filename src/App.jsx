96dd8f24097697812fa9511076a15f873e3cfa4a
zak-opus <1958575745@qq.com>
2022-04-16 22:41:54 +0800

    路由懒加载

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { Component } from "react";
-import { Routes, Route, Navigate, NavLink,Link } from "react-router-dom";
+import { Component, Suspense } from "react";
+import { NavLink, Routes, Route, Navigate, Link } from "react-router-dom";
 import routes from "./config/routes";
+import Loading from "./pages/loading";
 export default class App extends Component {
   render() {
     return (
       <div>
         <Link to="/user">User</Link>
         <NavLink to="/test1">Test1</NavLink>
-        <Routes>
-          {routes.map((routeObj) => (
-            <Route key={routeObj.path} {...routeObj} />
-          ))}
-          <Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
+        <Suspense fallback={<Loading></Loading>}>
+          <Routes>
+            {routes.map((routeObj) => (
+              <Route key={routeObj.path} {...routeObj} />
+            ))}
+            <Route path="/" element={<Navigate to="/login" />} />
+          </Routes>
+        </Suspense>
       </div>
     );
   }
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,13 @@
+import { lazy } from "react";
 // 配置路由信息
-import Login from "../pages/Login";
-// import User from "../pages/User";
-import User from "../container/Count";
-import Test1 from "../pages/Test1";
+// import Login from "../pages/Login";
+// // import User from "../pages/User";
+// import User from "../container/Count";
+// import Test1 from "../pages/Test1";
+// 懒加载
+const Login = lazy(() => import("../pages/Login"));
+const User = lazy(() => import("../container/Count"));
+const Test1 = lazy(() => import("../pages/Test1"));
 const routes = [
   {
     path: "/login",
